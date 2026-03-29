f757f7f7902573d1fe031240e17f45ceda0407cd
Marcus Merrell <mmerrell@gmail.com>
2026-03-28 17:20:02 -0700

    Fix: explicit 127.0.0.1:7233 HostPort, Go 1.23 in go.mod files

--- lab1-child-workflows/practice/cmd/starter/main.go
+++ lab1-child-workflows/practice/cmd/starter/main.go
@@ -11,7 +11,7 @@ import (
 )
 
 func main() {
-	c, err := client.Dial(client.Options{})
+	c, err := client.Dial(client.Options{HostPort: "127.0.0.1:7233"})
 	if err != nil {
 		log.Fatalln("Unable to create client:", err)
 	}
